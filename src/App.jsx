cf5ad1363b0ff6658cd9c04dea42f18ca75edc0a
Malyk99 <malykdorgham1@gmail.com>
2025-11-19 12:46:58 +0100

    learning components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+   <div>
+     <p>Hello World</p>
+     <Text string="Hello" />
+     <Text string="World"/>
+   </div>
+   
+  )
+}
+
+function Text({string}) {
+  return (
+    <div>
+      <h1>{string}</h1>
+    </div>
+  )
+}
+
+function Component() {
+  return (
+    //<div></div>//need to be a parent can't have more than one parent div in the return statement
+    //using fragment you can solve the issue
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div></div>
+      <div></div>
     </>
   )
 }
