bd38cf571afd6a69b012abb0a5a245addd800eb1
malyk99 <malykdorgham1@gmail.com>
2025-11-27 09:04:44 +0100

    Api fetch movie UseState in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,30 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
 import "../css/Home.css";
 
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const movies = [
-        { id: 1, title: "Inception", release_date: "2010" },
-        { id: 2, title: "The Dark Knight", release_date: "2008" },
-        { id: 3, title: "Interstellar", release_date: "2014" }
-    ];
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
+            } catch (error) {
+                console.log(error);
+                setError("Failed to fetch popular movies.");
+            } finally {
+                setLoading(false);
+            }
+        }
+    }, [])
+
+    //const movies = getPopularMovies(); get the movies from the API but iit does so over and over 
+
 
     const handleSearch = (e) => {
         e.preventDefault()
@@ -29,11 +44,18 @@ function Home() {
                 <button type="submit" className="search-button">Search</button>
 
             </form>
+
+                {error && <div className="error-message">{error}</div>}
+
+            {loading ? (
+                <div className="loading">Loading...</div> 
+            ) : (
             <div className="movies-grid">
                 {movies.map((movie) => (
                     <MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
+            )}
         </div>
     )
 }
